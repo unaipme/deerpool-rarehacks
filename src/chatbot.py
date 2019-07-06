afe7d8c722557b56cd2ed63e88d76edb64a5de92
Francisco Javier Jurado Morejo <beetelbrox@gmail.com>
2019-07-06 19:35:43 +0200

    First Commit

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,77 +1,101 @@
 # importing Telegram API
-import nltk
-import numpy as np
-import string
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-
-from nltk.tag.stanford import StanfordNERTagger
-from bert_serving.client import BertClient
 import csv
+import numpy as np
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+import finder
+from bert_serving.client import BertClient
 
-text1 = '''
-All happy families are alike; each unhappy family is unhappy in its own way. All was confusion in the Oblonskys’ house.  The wife had found out that the husband was having an affair with their former French governess, and had announced to the husband that she could not live in the same house with him.  This situation had continued for three days now, and was painfully felt by the couple themselves, as well as by all the members of the family and household.  They felt that there was no sense in their living together and that people who meet accidentally at any inn have more connection with each other than they, the members of the family and household of the Oblonskys.  The wife would not leave her rooms, the husband was away for the third day.  The children were running all over the house as if lost; the English governess quarreled with the housekeeper and wrote a note to a friend, asking her to find her a new place; the cook had already left the premises the day before, at dinner-time; the kitchen-maid and coachman had given notice.
-'''
-text = '''
-Mark who works at Yahoo and John who works at google decided to meet at New York City
-'''
+def location(bot, update):
+	print(update.message.location)
+	bot.send_message(chat_id=update.message.chat_id, text="")
+	poi = finder.find(lat=update.message.location.latitude, lng=update.message.location.longitude)
 
-# defining callback function for the /start command
-def tr(bot, update):
-    translator = Translator()
-    msg = update.message.text[3:] # delete "/tr "
-    msg_tr = translator.translate(msg).text
-    bot.send_message(chat_id=update.message.chat_id, text=msg_tr)
 
-# loading the access token from token.txt
-#TOKEN = open('token.txt').read().strip()
+def echo(bot, update):
+	print(update.message.text)
+	bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
-# call main Telegram objects
-#updater = Updater(token=TOKEN)
-#dispatcher = updater.dispatcher
+# TELEGRAM STUFF
+TOKEN_FILE = 'token.txt'
+TOKEN = None
+tel_updater = None
 
-# handling callbacks functions to the commands
-#dispatcher.add_handler(CommandHandler('tr', tr))
+# BERT STUFF
+bert_client = None
+questions = []
+answers = []
 
-# starting the bot
-#updater.start_polling()
-import os
-print(os.getcwd())
+# DATA FILES
+QUESTIONS_FILE = '../data/questions.csv'
+ANSWERS_FILE = '../data/answers.csv'
 
-def init():
-    pass
+first_message = True
+language = 'en'
+
+def com_handler(bot, update):
+	text = update.message.text
+	print('Original question: {}'.format(text))
+	doc_vecs = []
+	if first_message:
+		global language
+		language = update.message.from_user.language_code
+		doc_vecs = bert_client.encode([x[0] for x in questions])
+		global first_question
+		first_question = False
+	query_vec = bert_client.encode([text])[0]
+    # compute normalized dot product as score
+	topk = 5
+	score = np.sum(query_vec * doc_vecs, axis=1) / np.linalg.norm(doc_vecs, axis=1)
+	topk_idx = np.argsort(score)[::-1][:topk]
+	print(questions[topk_idx[0]][0])
+	for idx in topk_idx:
+		print('> %s\t%s' % (score[idx], questions[idx]))
+
+	bot.send_message(chat_id=update.message.chat_id, text=questions[topk_idx[0]][0])
+
+	#bot.send_contact(chat_id=update.message.chat_id, phone_number= "112", first_name="Teléfono de emergencias")
+	#location_keyboard = telegram.KeyboardButton(text="Share your location", request_location=True)
+	#custom_keyboard = [[location_keyboard]]
+	#inline_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+	#bot.send_message(chat_id=update.message.chat_id, text="Would you be so kind as to share your location?", reply_markup=inline_markup)
+
+def init_chatbot():
+	# Read the token file
+	with open(TOKEN_FILE) as f_token:
+		global TOKEN; TOKEN = f_token.readline().rstrip();
+		global tel_updater; tel_updater = Updater(token=TOKEN)
+		tel_updater.dispatcher.add_handler(CommandHandler('chatbot', com_handler))
+		tel_updater.dispatcher.add_handler(MessageHandler(Filters.location, location))
+		tel_updater.dispatcher.add_handler(MessageHandler(Filters.all, com_handler))
+
+def load_questions():
+	print('Loading questions...')
+	with open(QUESTIONS_FILE) as csv_questions:
+		readCSV = csv.reader(csv_questions, delimiter=';')
+		next(readCSV)
+		for line in readCSV:
+			questions.append( (line[1].rstrip(),int(line[0])))
+
+	print('Loading answers...')
+	with open(ANSWERS_FILE) as csv_answers:
+		readCSV = csv.reader(csv_answers, delimiter=';')
+		next(readCSV)
+		for line in readCSV:
+			answers.append(line[1].rstrip())
+
+## I DONT BELIEVE YOU, we need to answer that
+
+def init_text_parser():
+	global bert_client
+	bert_client = BertClient(port=5555, port_out=5556)
+	load_questions()
 
 def main():
-    bc = BertClient(port=5555, port_out=5556)
-    with open('../data/questions.csv') as csv_questions:
-        readCSV = csv.reader(csv_questions, delimiter=';')
-        next(readCSV)
-        questions = []
-        for line in readCSV:
-            questions.append(line[1].rstrip()+'?')
-
-    with open('../data/answers.csv') as csv_answers:
-        readCSV = csv.reader(csv_answers, delimiter=';')
-        next(readCSV)
-        answers = []
-        for line in readCSV:
-            answers.append(line[1].rstrip())
-
-    print(questions)
-    doc_vecs = bc.encode(questions)
-    print(doc_vecs)
-
-    topk = 5
-
-    while True:
-        query = input('your question: ')
-        query_vec = bc.encode([query])[0]
-        # compute normalized dot product as score
-        score = np.sum(query_vec * doc_vecs, axis=1) / np.linalg.norm(doc_vecs, axis=1)
-        topk_idx = np.argsort(score)[::-1][:topk]
-        for idx in topk_idx:
-            print('> %s\t%s' % (score[idx], questions[idx]))
+	init_chatbot()
+	init_text_parser()
+	tel_updater.start_polling(bootstrap_retries=0)
 
 
 if __name__ == '__main__':
-    main()
+	main()
